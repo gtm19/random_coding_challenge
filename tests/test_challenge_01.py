4318a7029845c2e86008fab88fd62da52555a57d
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2023-07-05 14:35:15 +0100

    add additional tests

--- tests/test_challenge_01.py
+++ tests/test_challenge_01.py
@@ -1,4 +1,10 @@
 from random_coding_challenge.challenge_01 import challenge_01
 
-def test_example_1():
+
+def test_challenge_01():
+    # for 10:
+    # 3, 5, 6, 9 / sum = 23
     assert challenge_01(10) == 23
+    # take 20:
+    # 3, 5, 6, 9, 10, 12, 15, 18 / sum = 78
+    assert challenge_01(20) == 78
