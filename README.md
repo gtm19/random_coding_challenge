e4293a625159d9fd579f9b0a611e26ec0d7e9ff2
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2023-07-05 09:39:19 +0100

    challenge_01 done

---
+++ README.md
@@ -0,0 +1,3 @@
+## Random Python Coding Challenges
+
+This repository contains a collection of random Python coding challenges I've completed. Pretty self explanatory.
---
+++ ascot_random_coding_challenge/challenge_01.py
@@ -0,0 +1,22 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9.
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+def challenge_01(number: int) -> int:
+    """
+    Calculates the sum of all multiples of 3 and 5 less than a given number
+
+    Args:
+        number (int): The given number
+
+    Returns:
+        int: The sum of all multiples of 3 and 5 less than `number`
+    """
+    threes = range(3, number, 3)
+    fives = range(5, number , 5)
+    return sum(threes) + sum(fives)
+
+if __name__ == "__main__":
+    NUMBER = 1000
+    print(f"The answer for challenge 1 where {NUMBER=} is:")
+    print(challenge_01(NUMBER))
